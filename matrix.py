72fb495187490c99fff31bb33568d0a66ad6e3fb
ShaindelSimon <129065456+ShaindelSimon@users.noreply.github.com>
2024-02-13 19:10:13 +0200

    name

--- matrix.py
+++ matrix.py
@@ -1,3 +1,4 @@
+#Shain Simon
 m1 = [
     [1, 2],
     [3, 4],
@@ -7,8 +8,10 @@ m2 = [
     [10, 20],
     [30, 40],
 ]
+
 print(m1)
 print(m2)
+
 for row in range(2):
     for col in range(2):
         print(m1[row][col] + m2[row][col], end=" ")
